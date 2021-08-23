4ba3598211c6fbc78910fd111cf61163f59105e0
IlyaSteik <ilya.steik@mail.ru>
2021-08-23 17:56:51 +0300

    first commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.734fd39993e7f0b85c94b07fe9d07f56.js"
+  "./precache-manifest.27beb8d8ab982b44c37090dce659da05.js"
 );
 
 self.addEventListener('message', (event) => {
